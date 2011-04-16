441f9471419b8757f34088134d99f71f2d6006bf
Rob Cowie <rob_cowie@mac.com>
2011-04-16 19:59:47 +0100

    Initial commit

---
+++ jquery.stopwatch.js
@@ -0,0 +1,121 @@
+/*
+*/
+
+(function( $ ){
+
+    function incrementer(ct, increment) {
+        return function() { ct+=increment; return ct; };
+    }
+    
+    function pad2(number) {
+         return (number < 10 ? '0' : '') + number;
+    }
+    
+    function formatMilliseconds(millis) {
+        var d = new Date(millis);
+        return pad2(d.getHours()) + ':' + pad2(d.getMinutes()) + ':' + pad2(d.getSeconds());
+    }
+    
+    var methods = {
+        
+        init: function(options) {
+            var settings = {
+                updateInterval: 1000, 
+                startTime: 0, 
+                formatter: formatMilliseconds
+            };
+            
+            if (options) { $.extend(settings, options); }
+            
+            return this.each(function() {
+                var $this = $(this),
+                    data = $this.data('stopwatch');
+                
+                // If the plugin hasn't been initialized yet
+                if (!data) {
+                    // Setup the stopwatch data
+                    data = settings;
+                    data.target = $this;
+                    // create counter
+                    data.incrementer = incrementer(data.startTime, data.updateInterval);
+                    data.tick_function = function() {
+                        var millis = data.incrementer();
+                        data.target.trigger('tick.stopwatch', [millis]);
+                        data.target.stopwatch('render', millis);
+                    };
+                    $this.data('stopwatch', data);
+                }
+                
+            });
+        },
+        
+        start: function() {
+            return this.each(function() {
+                var $this = $(this),
+                    data = $this.data('stopwatch');
+                // Mark as active
+                data.active = true;
+                data.timerID = setInterval(data.tick_function, data.updateInterval)
+                $this.data('stopwatch', data);
+            });
+        },
+        
+        stop: function() {
+            return this.each(function() {
+                var $this = $(this),
+                    data = $this.data('stopwatch');
+                clearInterval(data.timerID);
+                data.active = false;
+                $this.data('stopwatch', data);
+            });
+        },
+        
+        destroy: function() {
+            return this.each(function(){
+                var $this = $(this),
+                    data = $this.data('stopwatch');
+                $this.stopwatch('stop').unbind('.stopwatch').removeData('stopwatch');                
+            })
+        },
+        
+        render: function(ct) {
+            var $this = $(this),
+                data = $this.data('stopwatch');
+            $this.html(data.formatter(ct));
+        },
+        
+        toggle: function() {
+            return this.each(function() {
+                var $this = $(this);
+                var data = $this.data('stopwatch');
+                if (data.active) {
+                    $this.stopwatch('stop');
+                } else {
+                    $this.stopwatch('start');
+                }
+            });
+        }, 
+        
+        reset: function() {
+            return this.each(function() {
+                var $this = $(this);
+                    data = $this.data('stopwatch');
+                data.incrementer = incrementer(data.startTime, data.updateInterval);
+                $this.data('stopwatch', data);
+            });
+        }
+    };
+    
+    
+    // Define the function
+    $.fn.stopwatch = function( method ) {
+        if (methods[method]) {
+            return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+        } else if (typeof method === 'object' || !method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error( 'Method ' +  method + ' does not exist on jQuery.stopwatch' );
+        } 
+    };
+
+})( jQuery );
